fc68cb728c2197213791ffbd0eed1c62ef9fc93a
Benjamin Smith <46410142+benyamindsmith@users.noreply.github.com>
2020-05-01 08:25:10 -0400

    Update WeightConverter.py

--- WeightConverter.py
+++ WeightConverter.py
@@ -1,19 +1,16 @@
-#Weight Converter (lbs to kg and vice versa)
-
 weight=int(input('''
 What is your weight (enter number)
-
 Weight: '''))
 
 quantifier=str(input('''
 (K)g or (l)bs? : '''))
 
-if quantifier=='K' or quantifier=='k' or quantifier=='Kg' or quantifier=='KG' or quantifier=="kg":
+if quantifier.upper()=='K'  or quantifier.upper()=='KG':
     convert=weight*1.45359237
     convert=round(convert,2)
     convert=str(convert)
     print(f"\nYour weight (converted to lbs) is {convert} lbs.")
-elif quantifier=="l" or quantifier=="L" or quantifier=="lbs" or quantifier=="LBS":
+elif quantifier.upper()=="L" or quantifier.upper()=="LB" or quantifier.upper=="LBS":
     convert=weight*0.45359237
     convert=round(convert,2)
     convert=str(convert)
