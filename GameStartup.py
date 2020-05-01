1035583af7fc5353ba080e89620b2d5399b7624b
Benjamin Smith <46410142+benyamindsmith@users.noreply.github.com>
2020-05-01 10:57:40 -0400

    Update GameStartup.py

--- GameStartup.py
+++ GameStartup.py
@@ -2,9 +2,7 @@
 
 print('''
 Welcome to Ben's Game Startup Engine!
-
 Type 'help' in the console for instruction:
-
 ''')
 
 user_input=input('> ')
@@ -12,27 +10,34 @@ user_input=input('> ')
 while True:
     if user_input=='start':
         print('''
-        The game has been started!
+The game has been started!
         ''')
+        last_user_input=user_input
         user_input=input('> ')
+        if user_input==last_user_input:
+            print("Hey! The Game has already been started! What are you doing??")
+            user_input=input('>')
     elif user_input.lower()=='help':
                 print('''
-        Type 'start' to start the game
-
-        Type 'stop' to pause the game
-
-        Type 'quit' to quit the game
+Type 'start' to start the game
+Type 'stop' to pause the game
+Type 'quit' to quit the game
         ''')
                 user_input=input('> ')
     elif user_input=='stop':
         print('''
-        Game paused
+Game paused
         ''')
+        last_user_input=user_input
         user_input=input('> ')
+        if user_input==last_user_input:
+            print("Hey! The Game has already been stopped! What are you doing??")
+            user_input=input('> ')
     elif user_input=='quit':
         
         print('''
-        Game exited! Thank you for playing!''')
+Game exited! Thank you for playing!
+''')
         break 
     else: 
         print("sorry I do not understand that")
