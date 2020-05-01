455326eb81b781c8d787d8edaa9c84acf0865312
Benjamin Smith <46410142+benyamindsmith@users.noreply.github.com>
2020-05-01 10:12:00 -0400

    Update GameStartup.py

--- GameStartup.py
+++ GameStartup.py
@@ -9,11 +9,12 @@ Type 'help' in the console for instruction:
 
 user_input=input('> ')
 
-while user_input !='quit':
+while True:
     if user_input=='start':
         print('''
         The game has been started!
         ''')
+        user_input=input('> ')
     elif user_input.lower()=='help':
                 print('''
         Type 'start' to start the game
@@ -22,11 +23,17 @@ while user_input !='quit':
 
         Type 'quit' to quit the game
         ''')
+                user_input=input('> ')
     elif user_input=='stop':
         print('''
         Game paused
         ''')
-    user_input=input('> ')
-else: 
+        user_input=input('> ')
+    elif user_input=='quit':
+        
         print('''
         Game exited! Thank you for playing!''')
+        break 
+    else: 
+        print("sorry I do not understand that")
+        user_input=input('> ')
