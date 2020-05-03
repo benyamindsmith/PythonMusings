d6b5fc7f9fd8a7023be309dcb31a0600c0a5f23d
Benjamin Smith <46410142+benyamindsmith@users.noreply.github.com>
2020-05-03 13:15:29 -0400

    Create ArithematicOperators.py

---
+++ hackerrankchallenges/ArithematicOperators.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+
+def operators(num1,num2):
+    print(sum([num1,num2]))
+    print(num1-num2)
+    print(num1*num2)
+
+operators(a,b)
