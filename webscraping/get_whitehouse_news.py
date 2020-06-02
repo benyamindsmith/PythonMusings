f7c0f98bcff16f6c425de8e9cc3acca92376ce8a
Benjamin Smith <46410142+benyamindsmith@users.noreply.github.com>
2020-06-02 18:06:48 -0400

    Update get_whitehouse_news.py

--- webscraping/get_whitehouse_news.py
+++ webscraping/get_whitehouse_news.py
@@ -1,4 +1,9 @@
 # A simple webscraper providing a dataset of all Whitehouse Breifings
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import re
+
 
 def get_whitehouse_breifings():
     # Generalize to all pages
@@ -30,10 +35,12 @@ def get_whitehouse_breifings():
         sp = BeautifulSoup(link_content, 'lxml')
         h2_links = sp.find_all('h2')
         date_links = sp.find_all('p', {"class": "meta__date"})
+        breif_links = sp.find_all('div', {"class": "briefing-statement__content"})
 
         title = []
         urls = []
         date = []
+        breifing_type = []
         for i in h2_links:
             a_tag = i.find('a')
             urls.append(a_tag.attrs['href'])
@@ -41,8 +48,16 @@ def get_whitehouse_breifings():
         for j in date_links:
             d_tag = j.find('time')
             date.append(d_tag.get_text())
+        for k in breif_links:
+            b_tag = k.find('p')
+            b_tag = b_tag.get_text()
+            b_tag = re.sub('\\t', '', b_tag)
+            b_tag = re.sub('\\n', '', b_tag)
+            breifing_type.append(b_tag)
 
-        dt = pd.DataFrame(list(zip(title, date, urls)))
+        dt = pd.DataFrame(list(zip(date, title, urls, breifing_type)))
 
         dat = pd.concat([dat, dt])
-    return(dat)
+
+    dat.rename(columns={"Date": date, "Title": title, "URL": urls, "Issue Type": breifing_type})
+    return (dat)
