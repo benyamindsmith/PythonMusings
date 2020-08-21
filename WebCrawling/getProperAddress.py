cad2f603bcf0cd951c9ae136098d1079a8cc12d8
Benjamin Smith <46410142+benyamindsmith@users.noreply.github.com>
2020-08-21 17:51:20 -0400

    Create getProperAddress.py

---
+++ WebCrawling/getProperAddress.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from time import sleep
+
+# A tool used for "purifying" non-complete addresses by getting them from google maps.
+# not a perfect tool but definitely good for ad-hoc work where your client will say 
+#"Not all our addresses our properly formatted so you will have to figure out the route"
+
+
+def getProperAddress(driverPath, addressesToValidate):
+
+    browser = webdriver.Chrome(myDriverPath)
+
+    browser.get("https://www.google.com/maps")
+
+    sleep(2)
+    textBox = browser.find_element_by_xpath('//*[@id="searchboxinput"]')
+    textBox.click()
+    sleep(1)
+    textBox.send_keys("27 Michael Crt. Thornhill")
+
+    sleep(1)
+    searchMaps = browser.find_element_by_xpath('//*[@id="searchbox-searchbutton"]')
+    searchMaps.click()
+
+    sleep(2)
+    ProperAddress_1 = browser.find_element_by_xpath(
+        '//*[@id="pane"]/div/div[1]/div/div/div[2]/div[1]/div[1]/div[1]/h1/span[1]')
+    ProperAddress_2 = browser.find_element_by_xpath('//*[@id="pane"]/div/div[1]/div/div/div[2]/div[1]/div[1]/h2/span')
+
+    ProperAddress = ProperAddress_1.text + " " + ProperAddress_2.text
+
+    return ProperAddress
