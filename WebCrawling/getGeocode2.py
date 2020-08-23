04abdd2aef7b57403ce6762c5184ac140ec236c5
Benjamin Smith <46410142+benyamindsmith@users.noreply.github.com>
2020-08-23 14:51:08 -0400

    🗺uses geocode.xyz 🔭

---
+++ WebCrawling/getGeocode2.py
@@ -0,0 +1,25 @@
+
+def getGeocode2(driverPath, Address):
+    '''
+    A function for getting geocodes of a given address on https://geocode.xyz/ with Selenium
+    and is in compliance with the robots.txt (as of 8/23/2020)
+
+    Currently only compatible with ChromeDriver.
+
+    :param driverPath: path to ChromeDriver
+    :param Address: Address you are interested in searching.
+    '''
+    browser = webdriver.Chrome(driverPath)
+    browser.get("https://geocode.xyz/")
+    textBox = browser.find_element_by_xpath('//*[@id="hello"]/div/div/div/div[1]/div/div/form/div/div/div[1]/input')
+    textBox.click()
+    textBox.send_keys(Address)
+    enter =browser.find_element_by_xpath('//*[@id="hello"]/div/div/div/div[1]/div/div/form/div/input')
+    enter.click()
+    sleep(5)
+    try:
+        geoCode= browser.find_element_by_xpath('//*[@id="bg-text"]/small/a').text
+    except:
+        geoCode="Na"
+    return geoCode
+
