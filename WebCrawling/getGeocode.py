687199a87d80ecb17e1d0dbf95c5e27f79a5d1d1
Benjamin Smith <46410142+benyamindsmith@users.noreply.github.com>
2020-08-23 14:02:25 -0400

    This is for geocode.ca 🗺

---
+++ WebCrawling/getGeocode.py
@@ -0,0 +1,22 @@
+ef getGeocode(driverPath, Address):
+    '''
+    A function for getting geocodes of a given address on geocode.ca with Selenium.
+    Currently only compatible with ChromeDriver.
+
+    :param driverPath:
+    :param Address:
+    :return:
+    '''
+    browser = webdriver.Chrome(driverPath)
+    browser.get("https://geocoder.ca/")
+    sleep(2)
+    textBox = browser.find_element_by_class_name('input-block-level')
+    textBox.click()
+    textBox.send_keys(Address)
+    enter =browser.find_element_by_xpath('//*[@id="geocode"]/input[2]')
+    enter.click()
+    try:
+        geoCode= browser.find_element_by_xpath('/html/body/div[2]/table[2]/tbody/tr/td[2]/p/strong').text
+    except:
+        geoCode="Na"
+    return geoCode
