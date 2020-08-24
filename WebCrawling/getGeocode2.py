060f6a8a2782f44746c6ea8ac6b398a18a6cd122
Benjamin Smith <46410142+benyamindsmith@users.noreply.github.com>
2020-08-24 17:19:03 -0400

    Update getGeocode2.py

--- WebCrawling/getGeocode2.py
+++ WebCrawling/getGeocode2.py
@@ -14,13 +14,13 @@ def getGeocode2(driverPath, Address):
     textBox = browser.find_element_by_xpath('//*[@id="hello"]/div/div/div/div[1]/div/div/form/div/div/div[1]/input')
     textBox.click()
     textBox.send_keys(Address)
-    enter =browser.find_element_by_xpath('//*[@id="hello"]/div/div/div/div[1]/div/div/form/div/input')
-    enter.click()
+    enter = browser.find_element_by_xpath('//*[@id="hello"]/div/div/div/div[1]/div/div/form/div/input')
     sleep(5)
+    enter.click()
     try:
-        geoCode= browser.find_element_by_xpath('//*[@id="bg-text"]/small/a').text
+        geoCode = browser.find_element_by_xpath('//*[@id="bg-text"]/small/a').text
         browser.close()
     except:
-        geoCode="Na"
+        geoCode = "Na"
         browser.close()
     return geoCode
