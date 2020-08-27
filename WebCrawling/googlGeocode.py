9457a74544948133df90a49fa1fa652f4f7e9c0a
Benjamin Smith <46410142+benyamindsmith@users.noreply.github.com>
2020-08-26 20:07:24 -0400

    Update googlGeocode.py

--- WebCrawling/googlGeocode.py
+++ WebCrawling/googlGeocode.py
@@ -4,7 +4,7 @@ import re
 
 def googlGeocode(driverPath, addressesToGeoCode):
     '''
-    A function for getting properly formatted addresses and based off of address by querying
+    A function for getting properly formatted addresses and geo-codes by querying
     Google Maps with Selenium. This is currently compatible with Chrome Driver only.
 
     :arg driverPath - path to your driver
@@ -22,11 +22,11 @@ def googlGeocode(driverPath, addressesToGeoCode):
         try:
             textBox.send_keys(addressesToGeoCode)
 
-            sleep(1)
+            sleep(2)
             searchMaps = browser.find_element_by_xpath('//*[@id="searchbox-searchbutton"]')
             searchMaps.click()
 
-            sleep(2)
+            sleep(4)
 
             ProperAddress_1 = browser.find_element_by_xpath(
                 '//*[@id="pane"]/div/div[1]/div/div/div[2]/div[1]/div[1]/div[1]/h1/span[1]')
@@ -36,7 +36,8 @@ def googlGeocode(driverPath, addressesToGeoCode):
             ProperAddress = ProperAddress_1.text + " " + ProperAddress_2.text
             geoCode=browser.current_url
 
-            geoCode=re.findall('@\d{1,3}\.\d{5,},-\d{1,3}\.\d{5,}',geoCode)
+            # Note- the regex is ad Hoc. Change it accordingly
+            geoCode=re.findall(r'@\d{1,3}\.\d{5,},-\d{1,3}\.\d{5,}',geoCode)
             browser.close()
 
         except:
@@ -54,5 +55,12 @@ def googlGeocode(driverPath, addressesToGeoCode):
         ProperAddress="Nan"
         geoCode="Nan"
 
-    return [ProperAddress,geoCode]
+    # Very Ad-Hoc. Returns data in list format therefore need to extract data
+    # and convert to float
+
+    Latitude=float(re.findall("(?<=@)\d{1,3}\.\d{5,}",str(geoCode))[0])
+    Longitude= float(re.findall("(?<=,)-\d{1,3}\.\d{5,}",str(geoCode))[0])
+    return [ProperAddress,Latitude,Longitude]
+
+
 
